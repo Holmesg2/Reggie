2969784551e76975cfb9e85d0f149b5d87c7c937
Holmesg2 <gilesholmespwns@gmail.com>
2018-04-04 04:45:26 -0400

    Update mainpage.js

--- mainpage.js
+++ mainpage.js
@@ -2,6 +2,8 @@ var y = document.getElementById("Course");
 var x = document.getElementById("Schedule");
 var z = document.getElementById("Audit");
 
+window.onload=updateSemester();
+
 function schedule(){
 	
 	y.style.display="none";
@@ -17,4 +19,25 @@ function audit(){
 	y.style.display="none";
 	z.style.display="block";
 	x.style.display="none";
+}
+
+
+function updateSemester(){
+	setTimeout(function(){
+		var sem=document.getElementById("sem");
+		var val = $(".carousel-item.active").attr('value');
+		if (val == -1){	
+			sem.innerHTML = 'Completed Courses';
+		}
+		else{
+			val++;
+			sem.innerHTML = 'Semester #' + val;
+		}
+    }, 670);
+}
+
+function updateSeminfo(i,seminfo){
+	var sem=document.getElementsByName("seminfo"+i);
+	alert(sem[0]);
+	sem[0].innerHTML = seminfo;
 }
