10b1cd1e04e1f3c3f7b2404b6393652068e0be93
Dylan Brennan <brennand@wit.edu>
2018-04-03 21:36:29 -0400

    clicking schedule opens schedule clicking browse opens courses

---
+++ mainpage.js
@@ -0,0 +1,18 @@
+var y = document.getElementById("Course");
+var x = document.getElementById("Schedule");
+
+function schedule(){
+	
+	//if(y.style.display==="none"){
+	y.style.display="none";
+	//}else{
+	x.style.display="block";
+	//}
+}
+function browseCourses(){
+	//if(y.style.display==="none"){
+	y.style.display="block";
+	//}else{
+	x.style.display="none";
+	//}
+}
