fdfdda5745c249cdcefd33eeea811bf4a46cbb1e
Dylan Brennan <brennand@wit.edu>
2018-04-06 00:22:09 -0400

    made all buttons default to same color, updated the carousel button function to handle completed courses being clicked as well.

--- mainpage.js
+++ mainpage.js
@@ -58,18 +58,30 @@ function updateSeminfo(i,seminfo){
 }
 
 function colorBtn(completed, position){
-	var temp;
-	temp = position - completed;
-	temp = temp % 4;
-	var head = positon-temp+1;
-	var toChange1 = '.btn-course'+head.toString();
-	var toChange2 = '.btn-course'+(head+1).toString();
-	var toChange3 = '.btn-course'+(head+2).toString();
-	var toChange4 = '.btn-course'+(head+3).toString();
-	var toSelected = '.btn-course'+(position).toString();
-	$(toChange1).css('background-color', 'white');
-	$(toChange2).css('background-color', 'white');
-	$(toChange3).css('background-color', 'white');
-	$(toChange4).css('background-color', 'white');
-	$(toSelected).css('background-color','#009100');
+	if(completed<position){
+		var temp;
+		temp = position - completed;
+		temp = temp % 4;
+		var head = positon-temp+1;
+		var toChange1 = '.btn-course'+head.toString();
+		var toChange2 = '.btn-course'+(head+1).toString();
+		var toChange3 = '.btn-course'+(head+2).toString();
+		var toChange4 = '.btn-course'+(head+3).toString();
+		var toSelected = '.btn-course'+(position).toString();
+		$(toChange1).css('background-color', 'white');
+		$(toChange2).css('background-color', 'white');
+		$(toChange3).css('background-color', 'white');
+		$(toChange4).css('background-color', 'white');
+		$(toSelected).css('background-color','#009100');
+	}
+	else{
+		for(int i=0; i<completed; i++){
+			$('.btn-course'+i.toString()).css('background-color', 'white');
+		}
+		$('.btn-course'+position.toString().css('background-color','#009100');
+	}
 }
+
+function colorCompleted(completed, position){
+
+
