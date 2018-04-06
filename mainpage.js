20e36e696b25965d8f0c1f6ef267f6afedbc874e
Dylan Brennan <brennand@wit.edu>
2018-04-06 00:03:10 -0400

    colorBtn(), untested. waiting for giles part to test.

--- mainpage.js
+++ mainpage.js
@@ -56,3 +56,20 @@ function updateSeminfo(i,seminfo){
 	alert(sem[0]);
 	sem[0].innerHTML = seminfo;
 }
+
+function colorBtn(completed, position){
+	var temp;
+	temp = position - completed;
+	temp = temp % 4;
+	var head = positon-temp+1;
+	var toChange1 = '.btn-course'+head.toString();
+	var toChange2 = '.btn-course'+(head+1).toString();
+	var toChange3 = '.btn-course'+(head+2).toString();
+	var toChange4 = '.btn-course'+(head+3).toString();
+	var toSelected = '.btn-course'+(position).toString();
+	$(toChange1).css('background-color', 'white');
+	$(toChange2).css('background-color', 'white');
+	$(toChange3).css('background-color', 'white');
+	$(toChange4).css('background-color', 'white');
+	$(toSelected).css('background-color','#009100');
+}
